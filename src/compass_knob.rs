bb9a36c446e8d8b08d080a3070f8c14edc162541
Nagy Tibor <xnagytibor@protonmail.com>
2022-05-23 13:16:33 +0200

    Better styling

--- src/compass_knob.rs
+++ src/compass_knob.rs
@@ -2,7 +2,7 @@ use std::f32::consts::TAU;
 
 use eframe::egui::{self, Ui};
 use eframe::emath::{normalized_angle, pos2, vec2, Align2};
-use eframe::epaint::{Color32, FontFamily, FontId, Shape, Stroke};
+use eframe::epaint::{FontFamily, FontId, Shape};
 
 pub struct CompassLabels<'a>(pub [&'a str; 4]);
 
@@ -30,9 +30,9 @@ pub fn compass_knob(
 
         ui.painter().rect(
             rect,
-            0.0,
-            ui.style().visuals.faint_bg_color,
-            ui.style().visuals.window_stroke(),
+            visuals.rounding,
+            ui.style().visuals.extreme_bg_color,
+            ui.style().visuals.noninteractive().fg_stroke,
         );
 
         ui.painter().add(Shape::convex_polygon(
@@ -44,6 +44,14 @@ pub fn compass_knob(
             visuals.bg_fill,
             visuals.fg_stroke,
         ));
+
+        ui.painter().text(
+            rect.center_top(),
+            Align2::CENTER_TOP,
+            format!("{}°", (((value.to_degrees() as isize) % 360) + 360) % 360),
+            FontId::new(height / 4.0, FontFamily::Proportional),
+            visuals.text_color(),
+        );
 
         for i in -2..=3 {
             let x = map_value_to_screen(TAU / 4.0 * i as f32);
@@ -62,14 +70,6 @@ pub fn compass_knob(
                 labels.0[((i + 4) % 4) as usize],
                 FontId::new(height / 4.0, FontFamily::Proportional),
                 ui.style().visuals.text_color(),
-            );
-
-            ui.painter().text(
-                rect.center_top(),
-                Align2::CENTER_TOP,
-                format!("{}°", (((value.to_degrees() as isize) % 360) + 360) % 360),
-                FontId::new(height / 4.0, FontFamily::Proportional),
-                visuals.text_color(),
             );
         }
     }
