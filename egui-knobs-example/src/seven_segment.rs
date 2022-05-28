08119816284a56294e1f337411ee34921be4ad1e
Nagy Tibor <xnagytibor@protonmail.com>
2022-05-29 00:53:52 +0200

    More work on seven_segment

--- egui-knobs-example/src/seven_segment.rs
+++ egui-knobs-example/src/seven_segment.rs
@@ -3,83 +3,92 @@ use eframe::emath::{pos2, vec2, Pos2};
 use eframe::epaint::{Color32, Shape, Stroke};
 
 pub fn seven_segment(ui: &mut Ui) -> Response {
+    let thickness = 32.0;
+    let spacing = 4.0;
+    let slant = 16.0;
+
     let desired_size = vec2(128.0, 256.0);
     let (rect, mut response) = ui.allocate_exact_size(desired_size, Sense::hover());
 
     ui.painter()
         .rect(rect, 0.0, Color32::RED, Stroke::new(2.0, Color32::BLUE));
 
-    let thickness = 32.0;
-    let spacing = 2.0;
     let (width, height) = (rect.width(), rect.height());
 
-    let p = |x, y| rect.center() + vec2(x, y);
+    //#[rustfmt::skip]
+    let segments: [Vec<Pos2>; 7] = {
+        let p =
+            |x, y| rect.center() + (vec2(x, y) - vec2((y / (rect.height() / 2.0)) * slant, 0.0));
 
-    #[rustfmt::skip]
-    let segments: [Vec<Pos2>; 7] = [
-        vec![
-            p(-(width / 2.0 - thickness / 4.0 - spacing), -(height / 2.0 - thickness / 4.0)),
-            p(-(width / 2.0 - thickness / 2.0 - spacing), -(height / 2.0)),
-            p((width / 2.0 - thickness / 2.0 - spacing), -(height / 2.0)),
-            p((width / 2.0 - thickness / 4.0 - spacing), -(height / 2.0 - thickness / 4.0)),
-            p((width / 2.0 - thickness - spacing), -(height / 2.0 - thickness)),
-            p(-(width / 2.0 - thickness - spacing), -(height / 2.0 - thickness)),
-        ],
-        vec![
-            p((width / 2.0 - thickness), -(height / 2.0 - thickness - spacing)),
-            p((width / 2.0 - thickness / 4.0), -(height / 2.0 - thickness / 4.0 - spacing)),
-            p((width / 2.0), -(height / 2.0 - thickness / 2.0 - spacing)),
-            p((width / 2.0), -(thickness / 2.0 + spacing)),
-            p((width / 2.0 - thickness / 2.0), -(spacing)),
-            p((width / 2.0 - thickness), -(thickness / 2.0 + spacing)),
-        ],
-        vec![
-            p((width / 2.0 - thickness), (height / 2.0 - thickness - spacing)),
-            p((width / 2.0 - thickness / 4.0), (height / 2.0 - thickness / 4.0 - spacing)),
-            p((width / 2.0), (height / 2.0 - thickness / 2.0 - spacing)),
-            p((width / 2.0), (thickness / 2.0 + spacing)),
-            p((width / 2.0 - thickness / 2.0), (spacing)),
-            p((width / 2.0 - thickness), (thickness / 2.0 + spacing)),
-        ],
-        vec![
-            p(-(width / 2.0 - thickness / 4.0 - spacing), (height / 2.0 - thickness / 4.0)),
-            p(-(width / 2.0 - thickness / 2.0 - spacing), (height / 2.0)),
-            p((width / 2.0 - thickness / 2.0 - spacing), (height / 2.0)),
-            p((width / 2.0 - thickness / 4.0 - spacing), (height / 2.0 - thickness / 4.0)),
-            p((width / 2.0 - thickness - spacing), (height / 2.0 - thickness)),
-            p(-(width / 2.0 - thickness - spacing), (height / 2.0 - thickness)),
-        ],
-        vec![
-            p(-(width / 2.0 - thickness), (height / 2.0 - thickness - spacing)),
-            p(-(width / 2.0 - thickness / 4.0), (height / 2.0 - thickness / 4.0 - spacing)),
-            p(-(width / 2.0), (height / 2.0 - thickness / 2.0 - spacing)),
-            p(-(width / 2.0), (thickness / 2.0 + spacing)),
-            p(-(width / 2.0 - thickness / 2.0), (spacing)),
-            p(-(width / 2.0 - thickness), (thickness / 2.0 + spacing)),
-        ],
-        vec![
-            p(-(width / 2.0 - thickness), -(height / 2.0 - thickness - spacing)),
-            p(-(width / 2.0 - thickness / 4.0), -(height / 2.0 - thickness / 4.0 - spacing)),
-            p(-(width / 2.0), -(height / 2.0 - thickness / 2.0 - spacing)),
-            p(-(width / 2.0), -(thickness / 2.0 + spacing)),
-            p(-(width / 2.0 - thickness / 2.0), -(spacing)),
-            p(-(width / 2.0 - thickness), -(thickness / 2.0 + spacing)),
-        ],
-        vec![
-            p(-(width / 2.0 - thickness / 2.0 - spacing), 0.0),
-            p(-(width / 2.0 - thickness - spacing), -(thickness / 2.0)),
-            p((width / 2.0 - thickness - spacing), -(thickness / 2.0)),
-            p((width / 2.0 - thickness / 2.0 - spacing), 0.0),
-            p((width / 2.0 - thickness - spacing), (thickness / 2.0)),
-            p(-(width / 2.0 - thickness - spacing), (thickness / 2.0)),
-        ],
-    ];
+        let (w2, h2) = (width / 2.0, height / 2.0);
+        let (t1, t2, t4) = (thickness / 1.0, thickness / 2.0, thickness / 4.0);
+        let sp = spacing;
+
+        [
+            vec![
+                p(-(w2 - t4 - sp), -(h2 - t4)),
+                p(-(w2 - t2 - sp), -(h2)),
+                p((w2 - t2 - sp), -(h2)),
+                p((w2 - t4 - sp), -(h2 - t4)),
+                p((w2 - t1 - sp), -(h2 - t1)),
+                p(-(w2 - t1 - sp), -(h2 - t1)),
+            ],
+            vec![
+                p((w2 - t1), -(h2 - t1 - sp)),
+                p((w2 - t4), -(h2 - t4 - sp)),
+                p((w2), -(h2 - t2 - sp)),
+                p((w2), -(t2 + sp)),
+                p((w2 - t2), -(sp)),
+                p((w2 - t1), -(t2 + sp)),
+            ],
+            vec![
+                p((w2 - t1), (h2 - t1 - sp)),
+                p((w2 - t4), (h2 - t4 - sp)),
+                p((w2), (h2 - t2 - sp)),
+                p((w2), (t2 + sp)),
+                p((w2 - t2), (sp)),
+                p((w2 - t1), (t2 + sp)),
+            ],
+            vec![
+                p(-(w2 - t4 - sp), (h2 - t4)),
+                p(-(w2 - t2 - sp), (h2)),
+                p((w2 - t2 - sp), (h2)),
+                p((w2 - t4 - sp), (h2 - t4)),
+                p((w2 - t1 - sp), (h2 - t1)),
+                p(-(w2 - t1 - sp), (h2 - t1)),
+            ],
+            vec![
+                p(-(w2 - t1), (h2 - t1 - sp)),
+                p(-(w2 - t4), (h2 - t4 - sp)),
+                p(-(w2), (h2 - t2 - sp)),
+                p(-(w2), (t2 + sp)),
+                p(-(w2 - t2), (sp)),
+                p(-(w2 - t1), (t2 + sp)),
+            ],
+            vec![
+                p(-(w2 - t1), -(h2 - t1 - sp)),
+                p(-(w2 - t4), -(h2 - t4 - sp)),
+                p(-(w2), -(h2 - t2 - sp)),
+                p(-(w2), -(t2 + sp)),
+                p(-(w2 - t2), -(sp)),
+                p(-(w2 - t1), -(t2 + sp)),
+            ],
+            vec![
+                p(-(w2 - t2 - sp), 0.0),
+                p(-(w2 - t1 - sp), -(t2)),
+                p((w2 - t1 - sp), -(t2)),
+                p((w2 - t2 - sp), 0.0),
+                p((w2 - t1 - sp), (t2)),
+                p(-(w2 - t1 - sp), (t2)),
+            ],
+        ]
+    };
 
     for segment in segments {
         ui.painter().add(Shape::convex_polygon(
             segment,
             Color32::GREEN,
-            Stroke::none(),
+            Stroke::new(2.0, Color32::DARK_GREEN),
         ));
     }
 
