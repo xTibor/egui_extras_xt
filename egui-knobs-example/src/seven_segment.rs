3dbeec37035ac5e47cee20b881e18a7a37cf1288
Nagy Tibor <xnagytibor@protonmail.com>
2022-05-29 01:40:35 +0200

    More work on seven_segment

--- egui-knobs-example/src/seven_segment.rs
+++ egui-knobs-example/src/seven_segment.rs
@@ -11,71 +11,68 @@ pub fn seven_segment(ui: &mut Ui, thickness: f32, spacing: f32, slant: f32) -> R
 
     let (width, height) = (rect.width(), rect.height());
 
-    //#[rustfmt::skip]
+    #[rustfmt::skip]
     let segments: [Vec<Pos2>; 7] = {
         let p =
             |x, y| rect.center() + (vec2(x, y) - vec2((y / (rect.height() / 2.0)) * slant, 0.0));
 
-        let (w2, h2) = (width / 2.0, height / 2.0);
-        let (t1, t2, t4) = (thickness / 1.0, thickness / 2.0, thickness / 4.0);
-        let sp = spacing;
-
+        #[allow(unused_parens)]
         [
             vec![
-                p(-w2 + t4 + sp, -h2 + t4),
-                p(-w2 + t2 + sp, -h2),
-                p(w2 - t2 - sp, -h2),
-                p(w2 - t4 - sp, -h2 + t4),
-                p(w2 - t1 - sp, -h2 + t1),
-                p(-w2 + t1 + sp, -h2 + t1),
+                p(-(width / 2.0) + (thickness / 4.0) + spacing, -(height / 2.0) + (thickness / 4.0)          ),
+                p(-(width / 2.0) + (thickness / 2.0) + spacing, -(height / 2.0)                              ),
+                p( (width / 2.0) - (thickness / 2.0) - spacing, -(height / 2.0)                              ),
+                p( (width / 2.0) - (thickness / 4.0) - spacing, -(height / 2.0) + (thickness / 4.0)          ),
+                p( (width / 2.0) - (thickness / 1.0) - spacing, -(height / 2.0) + (thickness / 1.0)          ),
+                p(-(width / 2.0) + (thickness / 1.0) + spacing, -(height / 2.0) + (thickness / 1.0)          ),
             ],
             vec![
-                p(w2 - t1, -h2 + t1 + sp),
-                p(w2 - t4, -h2 + t4 + sp),
-                p(w2, -h2 + t2 + sp),
-                p(w2, -t2 - sp),
-                p(w2 - t2, -sp),
-                p(w2 - t1, -t2 - sp),
+                p( (width / 2.0) - (thickness / 1.0)          , -(height / 2.0) + (thickness / 1.0) + spacing),
+                p( (width / 2.0) - (thickness / 4.0)          , -(height / 2.0) + (thickness / 4.0) + spacing),
+                p( (width / 2.0)                              , -(height / 2.0) + (thickness / 2.0) + spacing),
+                p( (width / 2.0)                              ,                 - (thickness / 2.0) - spacing),
+                p( (width / 2.0) - (thickness / 2.0)          ,                                     - spacing),
+                p( (width / 2.0) - (thickness / 1.0)          ,                 - (thickness / 2.0) - spacing),
             ],
             vec![
-                p(w2 - t1, h2 - t1 - sp),
-                p(w2 - t4, h2 - t4 - sp),
-                p(w2, h2 - t2 - sp),
-                p(w2, t2 + sp),
-                p(w2 - t2, sp),
-                p(w2 - t1, t2 + sp),
+                p( (width / 2.0) - (thickness / 1.0)          ,  (height / 2.0) - (thickness / 1.0) - spacing),
+                p( (width / 2.0) - (thickness / 4.0)          ,  (height / 2.0) - (thickness / 4.0) - spacing),
+                p( (width / 2.0)                              ,  (height / 2.0) - (thickness / 2.0) - spacing),
+                p( (width / 2.0)                              ,                   (thickness / 2.0) + spacing),
+                p( (width / 2.0) - (thickness / 2.0)          ,                                       spacing),
+                p( (width / 2.0) - (thickness / 1.0)          ,                   (thickness / 2.0) + spacing),
             ],
             vec![
-                p(-w2 + t4 + sp, h2 - t4),
-                p(-w2 + t2 + sp, h2),
-                p(w2 - t2 - sp, h2),
-                p(w2 - t4 - sp, h2 - t4),
-                p(w2 - t1 - sp, h2 - t1),
-                p(-w2 + t1 + sp, h2 - t1),
+                p(-(width / 2.0) + (thickness / 4.0) + spacing,  (height / 2.0) - (thickness / 4.0)          ),
+                p(-(width / 2.0) + (thickness / 2.0) + spacing,  (height / 2.0)                              ),
+                p( (width / 2.0) - (thickness / 2.0) - spacing,  (height / 2.0)                              ),
+                p( (width / 2.0) - (thickness / 4.0) - spacing,  (height / 2.0) - (thickness / 4.0)          ),
+                p( (width / 2.0) - (thickness / 1.0) - spacing,  (height / 2.0) - (thickness / 1.0)          ),
+                p(-(width / 2.0) + (thickness / 1.0) + spacing,  (height / 2.0) - (thickness / 1.0)          ),
             ],
             vec![
-                p(-w2 + t1, h2 - t1 - sp),
-                p(-w2 + t4, h2 - t4 - sp),
-                p(-w2, h2 - t2 - sp),
-                p(-w2, t2 + sp),
-                p(-w2 + t2, sp),
-                p(-w2 + t1, t2 + sp),
+                p(-(width / 2.0) + (thickness / 1.0)          ,  (height / 2.0) - (thickness / 1.0) - spacing),
+                p(-(width / 2.0) + (thickness / 4.0)          ,  (height / 2.0) - (thickness / 4.0) - spacing),
+                p(-(width / 2.0)                              ,  (height / 2.0) - (thickness / 2.0) - spacing),
+                p(-(width / 2.0)                              ,                   (thickness / 2.0) + spacing),
+                p(-(width / 2.0) + (thickness / 2.0)          ,                                       spacing),
+                p(-(width / 2.0) + (thickness / 1.0)          ,                   (thickness / 2.0) + spacing),
             ],
             vec![
-                p(-w2 + t1, -h2 + t1 + sp),
-                p(-w2 + t4, -h2 + t4 + sp),
-                p(-w2, -h2 + t2 + sp),
-                p(-w2, -t2 - sp),
-                p(-w2 + t2, -sp),
-                p(-w2 + t1, -t2 - sp),
+                p(-(width / 2.0) + (thickness / 1.0)          , -(height / 2.0) + (thickness / 1.0) + spacing),
+                p(-(width / 2.0) + (thickness / 4.0)          , -(height / 2.0) + (thickness / 4.0) + spacing),
+                p(-(width / 2.0)                              , -(height / 2.0) + (thickness / 2.0) + spacing),
+                p(-(width / 2.0)                              ,                 - (thickness / 2.0) - spacing),
+                p(-(width / 2.0) + (thickness / 2.0)          ,                                     - spacing),
+                p(-(width / 2.0) + (thickness / 1.0)          ,                 - (thickness / 2.0) - spacing),
             ],
             vec![
-                p(-w2 + t2 + sp, 0.0),
-                p(-w2 + t1 + sp, -t2),
-                p(w2 - t1 - sp, -t2),
-                p(w2 - t2 - sp, 0.0),
-                p(w2 - t1 - sp, t2),
-                p(-w2 + t1 + sp, t2),
+                p(-(width / 2.0) + (thickness / 2.0) + spacing,                                           0.0),
+                p(-(width / 2.0) + (thickness / 1.0) + spacing,                 - (thickness / 2.0)          ),
+                p( (width / 2.0) - (thickness / 1.0) - spacing,                 - (thickness / 2.0)          ),
+                p( (width / 2.0) - (thickness / 2.0) - spacing,                                           0.0),
+                p( (width / 2.0) - (thickness / 1.0) - spacing,                   (thickness / 2.0)          ),
+                p(-(width / 2.0) + (thickness / 1.0) + spacing,                   (thickness / 2.0)          ),
             ],
         ]
     };
