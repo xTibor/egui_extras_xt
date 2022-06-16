acc8c1bf66d90ef95678ee6c5d7519bf0dce6ae6
Nagy Tibor <xnagytibor@protonmail.com>
2022-06-16 04:07:58 +0200

    Reorganize knob modules

---
+++ egui_extras_xt/src/knob/mod.rs
@@ -0,0 +1,2 @@
+pub mod angle_knob;
+pub mod audio_knob;
--- egui_extras_xt/src/lib.rs
+++ egui_extras_xt/src/lib.rs
@@ -1,12 +1,11 @@
-mod angle_knob;
-mod audio_knob;
 mod common;
 mod compass;
+mod knob;
 
-pub use angle_knob::{AngleKnob, AngleKnobPreset};
-pub use audio_knob::AudioKnob;
 pub use common::{Orientation, WidgetShape, WidgetShapeFn, Winding, WrapMode};
 pub use compass::linear_compass::{LinearCompass, LinearCompassMarker};
 pub use compass::{CompassLabels, CompassMarkerShape};
+pub use knob::angle_knob::{AngleKnob, AngleKnobPreset};
+pub use knob::audio_knob::AudioKnob;
 
 pub mod segmented_display;
