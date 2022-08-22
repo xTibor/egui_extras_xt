40ee4568c961302db2281f547929266ebabcb7c0
Nagy Tibor <xnagytibor@protonmail.com>
2022-08-22 15:37:10 +0200

    More work on BarcodeWidget

--- egui_extras_xt/src/barcodes/widget.rs
+++ egui_extras_xt/src/barcodes/widget.rs
@@ -1,29 +1,33 @@
-use egui::{pos2, vec2, Color32, Rect, Response, Sense, Stroke, Ui, Vec2, Widget};
+use egui::{vec2, Color32, Rect, Response, Sense, Stroke, Ui, Vec2, Widget};
 
 #[must_use = "You should put this widget in an ui with `ui.add(widget);`"]
 pub struct BarcodeWidget {
     value: String,
-    size: Vec2,
     padding: f32,
+    bar_width: f32,
+    bar_height: f32,
 }
 
 impl BarcodeWidget {
     pub fn new(value: &str) -> Self {
         Self {
             value: value.to_owned(),
-            size: vec2(256.0, 64.0),
-            padding: 16.0,
+            bar_width: 2.0,
+            bar_height: 64.0,
+            padding: 12.0,
         }
     }
 }
 
 impl Widget for BarcodeWidget {
     fn ui(self, ui: &mut Ui) -> Response {
-        let barcode = barcoders::sym::code39::Code39::new(&self.value)
-            .unwrap()
-            .encode();
+        let barcode = match barcoders::sym::code39::Code39::new(&self.value) {
+            Ok(code39) => code39.encode(),
+            _ => Vec::new(),
+        };
 
-        let desired_size = self.size + Vec2::splat(self.padding) * 2.0;
+        let desired_size = vec2(self.bar_width * barcode.len() as f32, self.bar_height)
+            + Vec2::splat(self.padding) * 2.0;
 
         let (rect, response) = ui.allocate_exact_size(desired_size, Sense::hover());
 
@@ -35,17 +39,14 @@ impl Widget for BarcodeWidget {
                 Stroke::none(),
             );
 
-            let bar_width = self.size.x / barcode.len() as f32;
-            let bar_height = self.size.y;
-
             for (i, b) in barcode.into_iter().enumerate() {
                 match b {
                     0 => {}
                     1 => ui.painter().rect(
                         Rect::from_min_size(
                             rect.left_top()
-                                + vec2(self.padding + bar_width * i as f32, self.padding),
-                            vec2(bar_width, bar_height),
+                                + vec2(self.padding + self.bar_width * i as f32, self.padding),
+                            vec2(self.bar_width, self.bar_height),
                         ),
                         0.0,
                         Color32::BLACK,
