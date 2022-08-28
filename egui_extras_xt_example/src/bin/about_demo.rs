15b6e8d487e50a5f313c99c5437d000229a1de8a
Nagy Tibor <xnagytibor@protonmail.com>
2022-08-28 16:01:31 +0200

    Clean up about box code, respect noreply addresses

--- egui_extras_xt_example/src/bin/about_demo.rs
+++ egui_extras_xt_example/src/bin/about_demo.rs
@@ -3,17 +3,13 @@ use eframe::emath::vec2;
 use egui_extras_xt::ui::hyperlink_with_icon::HyperlinkWithIcon;
 
 struct CargoPkgInfo {
-    name: String,
-    version: String,
-    authors: Vec<(String, Option<String>)>,
-    description: Option<String>,
-    homepage: Option<String>,
-    repository: Option<String>,
-    license: Option<String>,
-}
-
-struct AboutDemoApp {
-    pkg_info: CargoPkgInfo,
+    name: &'static str,
+    version: &'static str,
+    authors: &'static str,
+    description: Option<&'static str>,
+    homepage: Option<&'static str>,
+    repository: Option<&'static str>,
+    license: Option<&'static str>,
 }
 
 macro_rules! option_env_some {
@@ -25,31 +21,40 @@ macro_rules! option_env_some {
     };
 }
 
+macro_rules! cargo_pkg_info {
+    ( ) => {
+        CargoPkgInfo {
+            name: env!("CARGO_PKG_NAME"),
+            version: env!("CARGO_PKG_VERSION"),
+            authors: env!("CARGO_PKG_AUTHORS"),
+            description: option_env_some!("CARGO_PKG_DESCRIPTION"),
+            license: option_env_some!("CARGO_PKG_LICENSE"),
+            homepage: option_env_some!("CARGO_PKG_HOMEPAGE"),
+            repository: option_env_some!("CARGO_PKG_REPOSITORY"),
+        }
+    };
+}
+
+impl CargoPkgInfo {
+    fn authors(&self) -> impl Iterator<Item = (&'static str, Option<&'static str>)> {
+        self.authors.split(':').map(|author_line| {
+            let author_parts = author_line
+                .split(|c| ['<', '>'].contains(&c))
+                .map(str::trim)
+                .collect::<Vec<_>>();
+            (author_parts[0], author_parts.get(1).cloned())
+        })
+    }
+}
+
+struct AboutDemoApp {
+    pkg_info: CargoPkgInfo,
+}
+
 impl Default for AboutDemoApp {
     fn default() -> Self {
         Self {
-            pkg_info: CargoPkgInfo {
-                name: env!("CARGO_PKG_NAME").to_owned(),
-                version: env!("CARGO_PKG_VERSION").to_owned(),
-                authors: env!("CARGO_PKG_AUTHORS")
-                    .split(':')
-                    .map(|author_line| {
-                        let author_parts = author_line
-                            .split(|c| ['<', '>'].contains(&c))
-                            .map(str::trim)
-                            .collect::<Vec<_>>();
-
-                        (
-                            author_parts[0].to_owned(),
-                            author_parts.get(1).map(|&s| s.to_owned()),
-                        )
-                    })
-                    .collect::<Vec<(String, Option<String>)>>(),
-                description: option_env_some!("CARGO_PKG_DESCRIPTION").map(|s| s.to_owned()),
-                license: option_env_some!("CARGO_PKG_LICENSE").map(|s| s.to_owned()),
-                homepage: option_env_some!("CARGO_PKG_HOMEPAGE").map(|s| s.to_owned()),
-                repository: option_env_some!("CARGO_PKG_REPOSITORY").map(|s| s.to_owned()),
-            },
+            pkg_info: cargo_pkg_info!(),
         }
     }
 }
@@ -59,22 +64,22 @@ impl eframe::App for AboutDemoApp {
         egui::CentralPanel::default().show(ctx, |ui| {});
 
         egui::Window::new("About").show(ctx, |ui| {
-            ui.heading(&self.pkg_info.name);
+            ui.heading(self.pkg_info.name);
             ui.label(format!("Version {}", self.pkg_info.version));
 
             ui.separator();
 
-            if let Some(description) = &self.pkg_info.description {
+            if let Some(description) = self.pkg_info.description {
                 ui.label(description);
                 ui.separator();
             }
 
             ui.horizontal(|ui| {
-                if let Some(homepage) = &self.pkg_info.homepage {
+                if let Some(homepage) = self.pkg_info.homepage {
                     ui.hyperlink_with_icon_to("Home page", homepage);
                 }
 
-                if let Some(repository) = &self.pkg_info.repository {
+                if let Some(repository) = self.pkg_info.repository {
                     ui.hyperlink_with_icon_to("Repository", repository);
                 }
             });
@@ -83,12 +88,19 @@ impl eframe::App for AboutDemoApp {
 
             ui.collapsing("Authors", |ui| {
                 ui.horizontal(|ui| {
-                    for (author_name, author_email) in &self.pkg_info.authors {
+                    for (author_name, author_email) in self.pkg_info.authors() {
                         if let Some(author_email) = author_email {
-                            ui.hyperlink_with_icon_to(
-                                author_name,
-                                format!("mailto:{author_email:}"),
-                            );
+                            if !["noreply@", "no-reply@", "@users.noreply."]
+                                .iter()
+                                .any(|no_reply| author_email.contains(no_reply))
+                            {
+                                ui.hyperlink_with_icon_to(
+                                    author_name,
+                                    format!("mailto:{author_email:}"),
+                                );
+                            } else {
+                                ui.label(author_name);
+                            }
                         } else {
                             ui.label(author_name);
                         }
