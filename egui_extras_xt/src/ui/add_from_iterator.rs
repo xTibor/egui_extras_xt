fe1eba624459776873b1be3615d5813c70b418b2
Nagy Tibor <xnagytibor@protonmail.com>
2022-08-31 02:07:23 +0200

    More work on AddFromIterator

--- egui_extras_xt/src/ui/add_from_iterator.rs
+++ egui_extras_xt/src/ui/add_from_iterator.rs
@@ -1,16 +1,32 @@
-use egui::Ui;
+use egui::{ComboBox, Ui, WidgetText};
 use std::fmt::Display;
 
-pub trait AddFromIterator {
-    fn selectable_value_from_iter<Value: PartialEq + Display + Copy>(
+pub trait AddFromIterator<Value> {
+    fn selectable_value_from_iter(
         &mut self,
+        current_value: &mut Value,
+        values: impl Iterator<Item = Value>,
+    );
+
+    fn radio_value_from_iter(
+        &mut self,
+        current_value: &mut Value,
+        values: impl Iterator<Item = Value>,
+    );
+
+    fn combobox_from_iter(
+        &mut self,
+        label: impl Into<WidgetText>,
         current_value: &mut Value,
         values: impl Iterator<Item = Value>,
     );
 }
 
-impl AddFromIterator for Ui {
-    fn selectable_value_from_iter<Value: PartialEq + Display + Copy>(
+impl<Value> AddFromIterator<Value> for Ui
+where
+    Value: PartialEq + Display + Copy,
+{
+    fn selectable_value_from_iter(
         &mut self,
         current_value: &mut Value,
         values: impl Iterator<Item = Value>,
@@ -18,5 +34,30 @@ impl AddFromIterator for Ui {
         for value in values {
             self.selectable_value(current_value, value, format!("{}", value));
         }
+    }
+
+    fn radio_value_from_iter(
+        &mut self,
+        current_value: &mut Value,
+        values: impl Iterator<Item = Value>,
+    ) {
+        for value in values {
+            self.radio_value(current_value, value, format!("{}", value));
+        }
+    }
+
+    fn combobox_from_iter(
+        &mut self,
+        label: impl Into<WidgetText>,
+        current_value: &mut Value,
+        values: impl Iterator<Item = Value>,
+    ) {
+        ComboBox::from_label(label)
+            .selected_text(format!("{}", current_value))
+            .show_ui(self, |ui| {
+                for value in values {
+                    ui.selectable_value(current_value, value, format!("{}", value));
+                }
+            });
     }
 }
