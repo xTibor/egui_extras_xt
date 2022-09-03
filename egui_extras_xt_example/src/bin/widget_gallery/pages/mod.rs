9de12544d69d947f6e9f836081cb400523967d83
Nagy Tibor <xnagytibor@protonmail.com>
2022-09-03 20:20:22 +0200

    Reorganize pages module

--- egui_extras_xt_example/src/bin/widget_gallery/pages/mod.rs
+++ egui_extras_xt_example/src/bin/widget_gallery/pages/mod.rs
@@ -1,49 +1,55 @@
-pub mod ui;
-
-pub mod angle_knob_page;
-pub mod audio_knob_page;
-pub mod barcode_page;
-pub mod datamatrix_page;
-pub mod hyperlink_with_icon_page;
-pub mod led_display_page;
-pub mod linear_compass_page;
-pub mod polar_compass_page;
-pub mod qrcode_page;
-pub mod segmented_display_page;
-pub mod thumbstick_knob_page;
-pub mod welcome_page;
+mod ui;
 
 use eframe::egui::Ui;
 use strum::{Display, EnumIter, EnumProperty};
+
+// ----------------------------------------------------------------------------
+
+mod angle_knob_page;
+use angle_knob_page::AngleKnobPage;
+
+mod audio_knob_page;
+use audio_knob_page::AudioKnobPage;
+
+mod barcode_page;
+use barcode_page::BarcodePage;
+
+mod datamatrix_page;
+use datamatrix_page::DataMatrixPage;
+
+mod hyperlink_with_icon_page;
+use hyperlink_with_icon_page::HyperlinkWithIconPage;
+
+mod led_display_page;
+use led_display_page::LedDisplayPage;
+
+mod linear_compass_page;
+use linear_compass_page::LinearCompassPage;
+
+mod polar_compass_page;
+use polar_compass_page::PolarCompassPage;
+
+mod qrcode_page;
+use qrcode_page::QrCodePage;
+
+mod segmented_display_page;
+use segmented_display_page::SegmentedDisplayPage;
+
+mod thumbstick_knob_page;
+use thumbstick_knob_page::ThumbstickKnobPage;
+
+mod welcome_page;
+use welcome_page::WelcomePage;
+
+// ----------------------------------------------------------------------------
 
 pub trait PageImpl {
     fn ui(&mut self, ui: &mut Ui);
 }
 
-// ----------------------------------------------------------------------------
-
 #[allow(clippy::enum_variant_names)]
 #[derive(Clone, Copy, Display, EnumIter, EnumProperty, Eq, Hash, PartialEq)]
 pub enum PageId {
-    #[strum(to_string = "Welcome")]
-    WelcomePage,
-
-    #[strum(to_string = "QrCodeWidget")]
-    #[strum(props(feature = "barcodes"))]
-    QrCodePage,
-
-    #[strum(to_string = "DataMatrixWidget")]
-    #[strum(props(feature = "barcodes"))]
-    DataMatrixPage,
-
-    #[strum(to_string = "BarcodeWidget")]
-    #[strum(props(feature = "barcodes"))]
-    BarcodePage,
-
-    #[strum(to_string = "HyperlinkWithIcon")]
-    #[strum(props(feature = "ui"))]
-    HyperlinkWithIconPage,
-
     #[strum(to_string = "AngleKnob")]
     #[strum(props(feature = "knobs"))]
     AngleKnobPage,
@@ -52,50 +58,61 @@ pub enum PageId {
     #[strum(props(feature = "knobs"))]
     AudioKnobPage,
 
-    #[strum(to_string = "ThumbstickKnob")]
-    #[strum(props(feature = "knobs"))]
-    ThumbstickKnobPage,
+    #[strum(to_string = "BarcodeWidget")]
+    #[strum(props(feature = "barcodes"))]
+    BarcodePage,
+
+    #[strum(to_string = "DataMatrixWidget")]
+    #[strum(props(feature = "barcodes"))]
+    DataMatrixPage,
+
+    #[strum(to_string = "HyperlinkWithIcon")]
+    #[strum(props(feature = "ui"))]
+    HyperlinkWithIconPage,
 
     #[strum(to_string = "LedDisplay")]
     #[strum(props(feature = "displays"))]
     LedDisplayPage,
 
-    #[strum(to_string = "SegmentedDisplayWidget")]
-    #[strum(props(feature = "displays"))]
-    SegmentedDisplayPage,
+    #[strum(to_string = "LinearCompass")]
+    #[strum(props(feature = "compasses"))]
+    LinearCompassPage,
 
     #[strum(to_string = "PolarCompass")]
     #[strum(props(feature = "compasses"))]
     PolarCompassPage,
 
-    #[strum(to_string = "LinearCompass")]
-    #[strum(props(feature = "compasses"))]
-    LinearCompassPage,
+    #[strum(to_string = "QrCodeWidget")]
+    #[strum(props(feature = "barcodes"))]
+    QrCodePage,
+
+    #[strum(to_string = "SegmentedDisplayWidget")]
+    #[strum(props(feature = "displays"))]
+    SegmentedDisplayPage,
+
+    #[strum(to_string = "ThumbstickKnob")]
+    #[strum(props(feature = "knobs"))]
+    ThumbstickKnobPage,
+
+    #[strum(to_string = "Welcome")]
+    WelcomePage,
 }
 
 impl PageId {
     pub fn create_page(&self) -> Box<dyn PageImpl> {
         match *self {
-            PageId::WelcomePage => Box::new(welcome_page::WelcomePage::default()),
-            PageId::QrCodePage => Box::new(qrcode_page::QrCodePage::default()),
-            PageId::DataMatrixPage => Box::new(datamatrix_page::DataMatrixPage::default()),
-            PageId::BarcodePage => Box::new(barcode_page::BarcodePage::default()),
-            PageId::HyperlinkWithIconPage => {
-                Box::new(hyperlink_with_icon_page::HyperlinkWithIconPage::default())
-            }
-            PageId::AngleKnobPage => Box::new(angle_knob_page::AngleKnobPage::default()),
-            PageId::AudioKnobPage => Box::new(audio_knob_page::AudioKnobPage::default()),
-            PageId::ThumbstickKnobPage => {
-                Box::new(thumbstick_knob_page::ThumbstickKnobPage::default())
-            }
-            PageId::LedDisplayPage => Box::new(led_display_page::LedDisplayPage::default()),
-            PageId::SegmentedDisplayPage => {
-                Box::new(segmented_display_page::SegmentedDisplayPage::default())
-            }
-            PageId::PolarCompassPage => Box::new(polar_compass_page::PolarCompassPage::default()),
-            PageId::LinearCompassPage => {
-                Box::new(linear_compass_page::LinearCompassPage::default())
-            }
+            PageId::AngleKnobPage => Box::new(AngleKnobPage::default()),
+            PageId::AudioKnobPage => Box::new(AudioKnobPage::default()),
+            PageId::BarcodePage => Box::new(BarcodePage::default()),
+            PageId::DataMatrixPage => Box::new(DataMatrixPage::default()),
+            PageId::HyperlinkWithIconPage => Box::new(HyperlinkWithIconPage::default()),
+            PageId::LedDisplayPage => Box::new(LedDisplayPage::default()),
+            PageId::LinearCompassPage => Box::new(LinearCompassPage::default()),
+            PageId::PolarCompassPage => Box::new(PolarCompassPage::default()),
+            PageId::QrCodePage => Box::new(QrCodePage::default()),
+            PageId::SegmentedDisplayPage => Box::new(SegmentedDisplayPage::default()),
+            PageId::ThumbstickKnobPage => Box::new(ThumbstickKnobPage::default()),
+            PageId::WelcomePage => Box::new(WelcomePage::default()),
         }
     }
 }
