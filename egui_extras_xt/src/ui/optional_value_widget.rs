52316b741d75c753306c413eea659f1803946e92
Nagy Tibor <xnagytibor@protonmail.com>
2022-09-03 02:10:13 +0200

    Allow non-numeric value types in OptionalValueWidget, force horizontal layout

--- egui_extras_xt/src/ui/optional_value_widget.rs
+++ egui_extras_xt/src/ui/optional_value_widget.rs
@@ -1,5 +1,4 @@
 use egui::{Response, Ui};
-use emath::Numeric;
 
 pub trait OptionalValueWidget<T> {
     fn optional_value_widget(
@@ -9,31 +8,33 @@ pub trait OptionalValueWidget<T> {
     ) -> Response;
 }
 
-impl<T: Numeric + Default> OptionalValueWidget<T> for Ui {
+impl<T: Default> OptionalValueWidget<T> for Ui {
     fn optional_value_widget(
         &mut self,
         value: &mut Option<T>,
         add_contents: impl FnOnce(&mut Self, &mut T) -> Response,
     ) -> Response {
         self.group(|ui| {
-            let mut checkbox_state = value.is_some();
-            ui.checkbox(&mut checkbox_state, "");
+            ui.horizontal(|ui| {
+                let mut checkbox_state = value.is_some();
+                ui.checkbox(&mut checkbox_state, "");
 
-            *value = match (value.is_some(), checkbox_state) {
-                (false, true) => Some(T::default()),
-                (true, false) => None,
-                _ => *value,
-            };
+                match (value.is_some(), checkbox_state) {
+                    (false, true) => *value = Some(T::default()),
+                    (true, false) => *value = None,
+                    _ => {}
+                };
 
-            match value {
-                Some(ref mut value) => {
-                    add_contents(ui, value);
+                match value {
+                    Some(ref mut value) => {
+                        add_contents(ui, value);
+                    }
+                    None => {
+                        let mut dummy_value = T::default();
+                        ui.add_enabled_ui(false, |ui| add_contents(ui, &mut dummy_value));
+                    }
                 }
-                None => {
-                    let mut dummy_value = T::default();
-                    ui.add_enabled_ui(false, |ui| add_contents(ui, &mut dummy_value));
-                }
-            }
+            })
         })
         .response
     }
