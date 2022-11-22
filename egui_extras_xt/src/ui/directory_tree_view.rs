8426651c5d5094fd5803a354a8f77e015465890d
Nagy Tibor <xnagytibor@protonmail.com>
2022-11-22 13:43:26 +0100

    Work on DirectoryTreeView path symbols

--- egui_extras_xt/src/ui/directory_tree_view.rs
+++ egui_extras_xt/src/ui/directory_tree_view.rs
@@ -1,3 +1,4 @@
+use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
 
 use egui::util::cache::{ComputerMut, FrameCache};
@@ -129,6 +130,58 @@ impl DirectoryTreeView for Ui {
             file_path: &Path,
         ) -> Option<Response> {
             let file_name = file_path.file_name().unwrap().to_str().unwrap();
+            let file_extension = file_path
+                .extension()
+                .map(OsStr::to_string_lossy)
+                .map(String::from);
+
+            let file_symbol = match file_extension.as_deref() {
+                // Plain text
+                Some(
+                    "txt" | "rs" | "log" | "toml" | "md" | "py" | "c" | "h" | "cpp" | "js" | "html"
+                    | "json" | "css" | "ini" | "inf" | "glsl",
+                ) => '\u{1F5B9}',
+                // Rich text
+                Some("pdf" | "rtf" | "doc" | "docx") => '\u{1F5BB}',
+                // Images
+                Some(
+                    "png" | "jpg" | "jpeg" | "bmp" | "kra" | "webp" | "gif" | "svg" | "ppm" | "pbm"
+                    | "qoi",
+                ) => '\u{1F5BC}',
+                // Video
+                Some("avi" | "mp4" | "webm" | "mkv" | "ogv") => '\u{1F39E}',
+                // Audio
+                Some(
+                    "mp3" | "flac" | "wav" | "m4a" | "ogg" | "opus" | "wma" | "mid" | "xm" | "mod"
+                    | "s3m" | "it" | "oga" | "mmp" | "mmpz" | "mscz",
+                ) => '\u{266B}',
+                // Archives
+                Some("zip" | "7z" | "rar" | "tar" | "gz" | "xz" | "cab" | "arj" | "wad") => {
+                    '\u{1F4E6}'
+                }
+                // SoundFont files
+                Some("sbk" | "sf2" | "sf3" | "sfark" | "dls") => '\u{1F4E6}',
+                // Compact disc images
+                Some("iso") => '\u{1F4BF}',
+                // Floppy disk images
+                Some("dsk" | "d64") => '\u{1F4BE}',
+                // Video game ROM images
+                Some("gba" | "cia" | "3ds" | "nds" | "nes" | "smc" | "sfc") => '\u{1F3AE}',
+                // Video game save files
+                Some("sav") => '\u{1F4BE}',
+                // Video game patch files
+                Some("ips" | "bps" | "ups") => '\u{229E}',
+                // Harddisk images
+                Some("hdm" | "hdi" | "vdi") => '\u{1F5B4}',
+                // Fonts
+                Some("ttf" | "otf" | "otb" | "woff" | "woff2") => '\u{1F5DB}',
+                // Executables
+                Some("appimage" | "sh" | "so" | "exe" | "com" | "dll" | "bat" | "love") => {
+                    '\u{2699}'
+                }
+                // Unknown
+                _ => '\u{1F5CB}',
+            };
 
             // egui bug (0.19.0): `selectable_value` returns `changed` responses
             // even when the supplied value has not changed when clicking the
@@ -136,7 +189,7 @@ impl DirectoryTreeView for Ui {
             Some(ui.selectable_value(
                 selected_path,
                 Some(file_path.to_path_buf()),
-                format!("\u{1F5CB} {file_name:}"),
+                format!("{file_symbol:} {file_name:}"),
             ))
         }
 
