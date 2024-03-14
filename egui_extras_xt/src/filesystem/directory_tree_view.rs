13a8d41d9468c88fa4a83a2bd398e9e60a47f9d5
Jakub Turowski <jakubturowski92@.com>
2024-03-14 16:09:21 +0100

    Update to egui 0.26.2, update dependencies

--- egui_extras_xt/src/filesystem/directory_tree_view.rs
+++ egui_extras_xt/src/filesystem/directory_tree_view.rs
@@ -225,8 +225,11 @@ impl<'a> DirectoryTreeViewWidget<'a> {
 
         if let Some((add_contents_fn, enabled_fn)) = &self.directory_context_menu {
             if enabled_fn(directory_path) {
-                header_response =
-                    header_response.context_menu(|ui| add_contents_fn(ui, directory_path));
+                header_response
+                    .context_menu(|ui| add_contents_fn(ui, directory_path))
+                    .map(|resp| {
+                        header_response = resp.response;
+                    });
             }
         }
 
@@ -330,7 +333,11 @@ impl<'a> DirectoryTreeViewWidget<'a> {
 
         if let Some((add_contents_fn, enabled_fn)) = &self.file_context_menu {
             if enabled_fn(file_path) {
-                response = response.context_menu(|ui| add_contents_fn(ui, file_path));
+                response
+                    .context_menu(|ui| add_contents_fn(ui, file_path))
+                    .map(|resp| {
+                        response = resp.response;
+                    });
             }
         }
 
